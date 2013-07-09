a674437152f7c5fe62e98860c6bd46aeb63684b5
Patrick Quirk <quirker@gmail.com>
2013-07-09 14:31:46 -0400

    Day start time implemented

--- Source/Timely.ViewModels/Main/StatusBarViewModel.cs
+++ Source/Timely.ViewModels/Main/StatusBarViewModel.cs
@@ -1,11 +1,28 @@
 ﻿namespace Timely.ViewModels.Main
 {
     using System;
+    using System.Linq;
     using GalaSoft.MvvmLight;
+    using Timely.Models.Common;
+    using Timely.Models.Models;
+    using Timely.ViewModels.Common;
 
     public class StatusBarViewModel : ViewModelBase, IStatusBarViewModel
     {
+        readonly IActiveTaskController activeTaskController;
         DateTime dayStartTime;
+
+        public StatusBarViewModel(ITimeBlocksModel timeBlocksModel, IActiveTaskController activeTaskController)
+        {
+            this.activeTaskController = activeTaskController;
+            SubscribeToActiveTaskEvents();
+            ExtractStartTime(timeBlocksModel);
+        }
+
+        void SubscribeToActiveTaskEvents()
+        {
+            activeTaskController.TaskStarted += HandleTaskStarted;
+        }
 
         public DateTime DayStartTime
         {
@@ -15,9 +32,23 @@
             }
             private set
             {
-                dayStartTime = value;
-                RaisePropertyChanged(() => dayStartTime);
+                if (dayStartTime != value)
+                {
+                    dayStartTime = value;
+                    RaisePropertyChanged(() => DayStartTime);
+                }
             }
+        }
+
+        void ExtractStartTime(ITimeBlocksModel timeBlocksModel)
+        {
+            DayStartTime = timeBlocksModel.GetAll().Select(t => t.Start.ToLocalTime()).FirstOrDefault(d => d.Date == DateTime.Today);
+        }
+
+        void HandleTaskStarted(object sender, EntityIdEventArgs e)
+        {
+            if (DayStartTime == DateTime.MinValue)
+                DayStartTime = DateTime.Now;
         }
     }
 }
