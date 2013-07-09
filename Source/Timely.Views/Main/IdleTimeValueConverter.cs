fd1d8b050eeb668f391ba9367f0c23253cfc1bed
Patrick Quirk <quirker@gmail.com>
2013-07-09 14:57:40 -0400

    Plugged in the idle time summer to the view

---
+++ Source/Timely.Views/Main/IdleTimeValueConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Timely.Views.Main
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class IdleTimeValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan idleTime = (TimeSpan)value;
+
+            if (idleTime > TimeSpan.Zero)
+                return "Idle: " + idleTime;
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
