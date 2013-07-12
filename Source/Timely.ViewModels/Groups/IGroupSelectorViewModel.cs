eb16e851671b9a67abd17c9b88aa10dde311ccf0
Patrick Quirk <quirker@gmail.com>
2013-07-11 20:02:37 -0400

    Added user control for a group selector

---
+++ Source/Timely.ViewModels/Groups/IGroupSelectorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Timely.ViewModels.Groups
+{
+    using System.Collections.ObjectModel;
+    using Timely.ViewModels.Base;
+
+    public interface IGroupSelectorViewModel : IViewModel
+    {
+        ObservableCollection<string> GroupNames { get; }
+    }
+}
---
+++ Source/Timely.ViewModels/Groups/SampleData/SampleGroupSelectorViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Timely.ViewModels.Groups.SampleData
+{
+    using System.Collections.ObjectModel;
+    using GalaSoft.MvvmLight;
+
+    public class SampleGroupSelectorViewModel : ViewModelBase, IGroupSelectorViewModel
+    {
+        public SampleGroupSelectorViewModel()
+        {
+            GroupNames = new ObservableCollection<string> { "Group 1", "Group 2", "Group 3" };
+        }
+
+        public ObservableCollection<string> GroupNames { get; private set; }
+    }
+}
