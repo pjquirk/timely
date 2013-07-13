1f19c615f7731485a55169a7c4d3f17ae65c62b4
Patrick Quirk <quirker@gmail.com>
2013-07-13 15:54:26 -0400

    Simple create group command

--- Source/Timely.ViewModels/Groups/GroupSelectorViewModel.cs
+++ Source/Timely.ViewModels/Groups/GroupSelectorViewModel.cs
@@ -4,6 +4,8 @@
     using System.Linq;
     using System.Windows.Input;
     using GalaSoft.MvvmLight;
+    using Timely.Models.Common;
+    using Timely.Models.Entities;
     using Timely.Models.Models;
 
     public class GroupSelectorViewModel : ViewModelBase, IGroupSelectorViewModel
@@ -14,12 +16,24 @@
         {
             NewGroupCommand = newGroupCommand;
             this.groupsModel = groupsModel;
+            this.groupsModel.EntityAdded += HandleEntityAdded;
+            this.groupsModel.EntityDeleted += HandleEntityDeleted;
             PopulateGroupNames();
         }
 
         public ObservableCollection<string> GroupNames { get; private set; }
 
         public ICommand NewGroupCommand { get; private set; }
+
+        void HandleEntityAdded(object sender, EntityEventArgs<Group> e)
+        {
+            GroupNames.Add(e.Entity.Name);
+        }
+
+        void HandleEntityDeleted(object sender, EntityIdEventArgs e)
+        {
+            // GroupNames.Remove(e.Entity.Name);
+        }
 
         void PopulateGroupNames()
         {
--- Source/Timely.ViewModels/Groups/NewGroupViewModel.cs
+++ Source/Timely.ViewModels/Groups/NewGroupViewModel.cs
@@ -1,12 +1,28 @@
 ﻿namespace Timely.ViewModels.Groups
 {
     using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Timely.Models.Models;
     using Timely.ViewModels.Base;
 
     public class NewGroupViewModel : ClosableViewModel, INewGroupViewModel
     {
+        readonly IGroupsModel groupsModel;
+
+        public NewGroupViewModel(IGroupsModel groupsModel)
+        {
+            this.groupsModel = groupsModel;
+            CreateGroupCommand = new RelayCommand(CreateGroupExecute);
+        }
+
         public ICommand CreateGroupCommand { get; private set; }
 
         public string GroupName { get; set; }
+
+        void CreateGroupExecute()
+        {
+            groupsModel.Add(GroupName);
+            Close();
+        }
     }
 }
